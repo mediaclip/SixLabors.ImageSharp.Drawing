94451691f1d2d7d7ba17da0d9c9d678604dbd829
Bertrand Provost <bertrand.provost@mediaclip.ca>
2025-12-15 15:32:05 -0500

    Update ci to publish internal package

--- ci-pack.ps1
+++ ci-pack.ps1
@@ -1,6 +1,11 @@
+param(
+  [Parameter(Mandatory = $true, Position = 0)]
+  [string]$Version
+)
+
 dotnet clean -c Release
 
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
 # Building for packing and publishing.
-dotnet pack -c Release --output "$PSScriptRoot/artifacts" /p:RepositoryUrl=$repositoryUrl
+dotnet pack -c Release --output "$PSScriptRoot/artifacts" /p:RepositoryUrl=$repositoryUrl -p:MinVerVersionOverride=$Version
